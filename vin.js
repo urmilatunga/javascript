3fedfa0e6b24e2927cbd9e3151c1e19c4355af2d
urmilatunga <urmilatunga22@gmail.com>
2022-04-21 10:45:52 +0530

    first commit

---
+++ vin.js
@@ -0,0 +1,51 @@
+function task1() {
+    let one = document.querySelector('#one');
+    let btn1 = document.querySelector('#btn1')
+    console.log(one.style.display)
+    if (one.style.display != "none") {
+        one.style.display = 'none'
+    }
+    else {
+        one.style.display = 'block'
+    }
+}
+function task2() {
+    let form2 = document.getElementById('two')
+    let btn2 = document.getElementById('btn2')
+    if (form2.style.display != 'none') {
+        form2.style.display = 'none'
+    }
+    else {
+        form2.style.display = 'block'
+    }
+}function task3() {
+    let form2 = document.getElementById('three')
+    let btn2 = document.getElementById('btn2')
+    if (form2.style.display != 'none') {
+        form2.style.display = 'none'
+    }
+    else {
+        form2.style.display = 'block'
+    }
+}
+function task4() {
+    let form2 = document.getElementById('four')
+    let btn2 = document.getElementById('btn2')
+    if (form2.style.display != 'none') {
+        form2.style.display = 'none'
+    }
+    else {
+        form2.style.display = 'block'
+    }
+
+}
+function task5() {
+    let form2 = document.getElementById('five')
+    let btn2 = document.getElementById('btn2')
+    if (form2.style.display != 'none') {
+        form2.style.display = 'none'
+    }
+    else {
+        form2.style.display = 'block'
+    }
+}
